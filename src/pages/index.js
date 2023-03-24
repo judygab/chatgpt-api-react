a09050afecc23b387605269be4c888a118b0d5dc
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-03-25 01:16:24 +0530

    fixed the messages overflowing issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto max-w-[700px]">
-      <div className="flex flex-col h-screen bg-gray-900">
+      <div className="flex flex-col min-h-screen bg-gray-900">
         <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">ChatGPT</h1>
         <div className="flex-grow p-6">
           <div className="flex flex-col space-y-4">
